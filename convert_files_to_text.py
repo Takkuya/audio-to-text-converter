69af0a444d1d89f42f78ba6f6d8b389d5eb791f6
Takkuya <takuyagabriel@gmail.com>
2023-03-24 14:20:07 -0300

    Saving .docx files in a folder

--- convert_files_to_text.py
+++ convert_files_to_text.py
@@ -5,18 +5,22 @@ from pydub import AudioSegment
 
 # lista todos os arquivos de áudio na pasta 'audio'
 audio_files = [f for f in os.listdir("audio") if f.endswith('.mp3')]
-# 
 
 # cria a pasta recordsWav caso não exista
 if not os.path.exists('recordsWav'):
     os.makedirs('recordsWav')
+
+# cria a pasta texto caso não exista
+if not os.path.exists('texto'):
+    os.makedirs('texto')
 
 # Reconhecimento de fala para cada arquivo de áudio na lista
 r = sr.Recognizer()
 for audio_file in audio_files:
     # convertendo o áudio mp3 para WAV e salvando em recordsWav
     sound = AudioSegment.from_mp3(os.path.join('audio', audio_file))
-    sound.export(os.path.join('recordsWav', audio_file.replace('.mp3', '.wav')), format="wav")
+    sound.export(os.path.join(
+        'recordsWav', audio_file.replace('.mp3', '.wav')), format="wav")
 
     # reconhecendo o áudio
     with sr.AudioFile(os.path.join('recordsWav', audio_file.replace('.mp3', '.wav'))) as source:
@@ -25,16 +29,11 @@ for audio_file in audio_files:
     try:
         # transformando o áudio em texto
         text = r.recognize_google(audio, language='pt-BR')
-        # print("Áudio: {}, Texto: {}".format(audio_file, text))
         print("Áudio: {} | Está sendo formatado...".format(audio_file, text))
 
-
-        # gera o arquivo .docx
-        filename = "texto.docx"
-        if not os.path.exists(filename):
-            doc = Document()
-            doc.save(filename)
-        doc = Document(filename)
+        # gera o arquivo .docx com o mesmo nome do arquivo de áudio
+        filename = os.path.join('texto', audio_file.replace('.mp3', '.docx'))
+        doc = Document()
         doc.add_paragraph(text)
         doc.save(filename)
 
